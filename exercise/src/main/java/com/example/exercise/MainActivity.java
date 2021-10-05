bcb6a51c21f13204aabac861289bb098b12e08b5
cuongSkedulo <cuong.nguyen@skedulo.com>
2021-10-05 23:42:35 +0700

    Finish student form

--- exercise/src/main/java/com/example/exercise/MainActivity.java
+++ exercise/src/main/java/com/example/exercise/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button form = (Button) findViewById(R.id.form);
+        form.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText name = (EditText) findViewById(R.id.nameInput);
+                Intent intent = new Intent(MainActivity.this, StudentForm.class);
+                intent.putExtra("name", name.getText().toString());
+
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            String successMessage = (String) intent.getExtras().get("response");
+            Toast.makeText(MainActivity.this, "Thank for your submission, "+successMessage, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- exercise/src/main/java/com/example/exercise/StudentForm.java
+++ exercise/src/main/java/com/example/exercise/StudentForm.java
@@ -2,7 +2,11 @@ package com.example.exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StudentForm extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class StudentForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_form);
+
+        Intent intent = getIntent();
+
+        TextView nameView = findViewById(R.id.nameView);
+        nameView.setText((String)intent.getExtras().get("name"));
+
+        Button button = (Button) findViewById(R.id.submit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(StudentForm.this, MainActivity.class);
+                intent1.putExtra("response", nameView.getText().toString());
+                setResult(RESULT_OK, intent1);
+                finish();
+            }
+        });
     }
 }
